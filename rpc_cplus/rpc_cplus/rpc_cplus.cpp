28c165bd3ac68f16166e1ff6eba2694987df35e9
vincent1000 <41051215+vincent1000@users.noreply.github.com>
2020-03-04 16:10:36 +0800

    Add files via upload

---
+++ rpc_cplus/rpc_cplus/rpc_cplus.cpp
@@ -0,0 +1,76 @@
+﻿// rpc_cplus.cpp 
+//
+#include <iostream>
+#include <Winsock2.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <Sddl.h>
+#include <rpcdce.h>
+#include "hello_h.h"
+using namespace std;
+
+#pragma comment(lib, "rpcrt4.lib")
+extern "C" {
+	void Hello(handle_t IDL_handle, const unsigned char* psz) {
+		printf("server:%s\n", psz);
+	}
+}
+
+void* __RPC_USER MIDL_user_allocate(size_t len)
+{
+	return (malloc(len));
+}
+
+void __RPC_USER MIDL_user_free(void* ptr)
+{
+	free(ptr);
+}
+
+inline ULONG BOOL_TO_ERROR(BOOL f)
+{
+	return f ? NOERROR : GetLastError();
+}
+
+ULONG InitRpcServer()
+{
+	PSECURITY_DESCRIPTOR SecurityDescriptor;
+
+	// generic all for SDDL_ALL_APP_PACKAGES + SDDL_EVERYONE
+
+	ULONG dwError = BOOL_TO_ERROR(ConvertStringSecurityDescriptorToSecurityDescriptorW(
+		L"D:P(A;;GA;;;WD)(A;;GA;;;AC)(A;;GA;;;S-1-15-2-2)S:(ML;;;;;LW)", SDDL_REVISION, &SecurityDescriptor, 0));
+
+	if (dwError == ERROR_SUCCESS)
+	{
+		dwError = RpcServerRegisterIf3(hello_v1_0_s_ifspec,
+			NULL, NULL, RPC_IF_ALLOW_CALLBACKS_WITH_NO_AUTH,
+			RPC_C_LISTEN_MAX_CALLS_DEFAULT, 0x10000, 0, SecurityDescriptor);
+
+		if (dwError == RPC_S_OK)
+		{
+			dwError = RpcServerUseProtseqEpW(
+				(RPC_WSTR)L"ncalrpc",
+				RPC_C_PROTSEQ_MAX_REQS_DEFAULT,
+				(RPC_WSTR)L"myname",
+				SecurityDescriptor);
+
+			if (dwError == RPC_S_OK)
+			{
+				dwError = RpcServerListen(1, RPC_C_LISTEN_MAX_CALLS_DEFAULT, false);
+				std::cout << "Client is comming!\n";
+			}
+		}
+
+		LocalFree(SecurityDescriptor);
+	}
+
+	return dwError;
+}
+
+int main()
+{
+	InitRpcServer();
+    std::cout << "Hello World!\n";
+}
+
---
+++ rpc_uwp/rpc_uwp/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace rpc_uwp
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        [DllImport("Rpcrt4.dll", CharSet = CharSet.Auto)]
+        public static extern int RpcBindingFromStringBinding(string StringBinding,out IntPtr Binding);
+
+        [DllImport("Rpcrt4.dll", CharSet = CharSet.Auto)]
+        public static extern int RpcBindingFree(ref IntPtr Binding);
+
+        [DllImport("Rpcrt4.dll", CharSet = CharSet.Auto)]
+        public static extern int RpcStringBindingCompose(string ObjUuid, string ProtSeq, string NetworkAddr, string EndPoint, string Options, out string StringBinding);
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            IntPtr bindingHandle = IntPtr.Zero;
+            string pszStringBinding;
+            RpcStringBindingCompose(
+                        null,
+                        "ncalrpc",
+                        null,
+                        "myname",
+                        null,
+                        out pszStringBinding);
+
+            int ret = RpcBindingFromStringBinding(pszStringBinding, out bindingHandle);
+            Console.WriteLine("Ret = " + ret);
+        }
+    }
+}
